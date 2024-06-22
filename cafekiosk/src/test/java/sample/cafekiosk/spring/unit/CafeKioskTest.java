3d4f6a8ebd74ef24df2e0162e783a2bd19030fd7
YouchanKim <dbcks4646@gmail.com>
2024-06-23 00:58:53 +0900

    junit 사용하여 단위테스트 기초

--- cafekiosk/src/test/java/sample/cafekiosk/spring/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/unit/CafeKioskTest.java
@@ -3,12 +3,13 @@ package sample.cafekiosk.spring.unit;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.spring.unit.beverage.Americano;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class CafeKioskTest {
 
     @Test
-    void add(){
+    void add_manual_test() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
@@ -20,6 +21,30 @@ class CafeKioskTest {
          * 1. 테스트 코드를 작성하였지만 최종 단계에서 사람의 확인이 필요(콘솔에 결과 출력)
          * 2. 타인이 보았을 때, 어떤 것을 검증해야 할 지 모름(틀린 상황인지도 확인 불가)
          */
+    }
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
 }
---
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/unit/beverage/AmericanoTest.java
@@ -0,0 +1,23 @@
+package sample.cafekiosk.spring.unit.beverage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmericanoTest {
+
+    @Test
+    void getName(){
+        Americano americano = new Americano();
+        assertEquals(americano.getName(), "아메리카노");             // junit
+        assertThat(americano.getName()).isEqualTo("아메리카노");  // assertj
+    }
+
+    @Test
+    void getPrice(){
+        Americano americano = new Americano();
+        assertThat(americano.getPrice()).isEqualTo(4000);
+    }
+
+}
